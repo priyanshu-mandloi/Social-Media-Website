f3e080632c9132348d51f69f98a9d0e1bef7b5dd
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-10 16:51:56 +0530

    First commit: create the directory structure and setup server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port =  8000;
+
+app.listen(port,function(err){
+ if(err){
+    // Normal way
+    // console.log("Error :",err);
+    // By interpolation method (use the tick)
+    console.log(`Error in running the server:  ${err}`);
+}
+
+ console.log(`Server is running on the port : ${port}`);
+});
