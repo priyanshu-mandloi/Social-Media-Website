f850bf8d5a5f8e5b0e9527f2ab2789199502be2a
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-24 19:33:00 +0530

    Making of the schema for comments

--- models/posts.js
+++ models/posts.js
@@ -8,7 +8,13 @@ const postSchema = new mongoose.Schema({
   user:{
     type:mongoose.Schema.Types.ObjectId,
     ref:'User'
-  }
+  },
+  comment:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'comment'
+    }
+]
 },{
     timestamps:true
 });
