35f4e11ce1c3037df9d71b483404cdf33c49db59
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-24 18:31:35 +0530

    Display the post on the webpage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,19 @@
-module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-        // return res.end('<h1>Express is up for codeial!</h1>');
-    return res.render('home',{
-        title:"Home"
-    });
-}
+const Post = require('../models/posts');
 
+module.exports.home = async function(req,res){
+    try{
+        // Populate the user of each post.
+     const posts = await Post.find({}).populate('user').exec();
+     const postArray = Array.isArray(posts) ? posts : [];
+     console.log(postArray);
+       return res.render('home',{
+          title:'Codeial | Home',
+          posts:postArray
+        }); 
+    }catch(err){
+        console.error(err);
+    }
+}
 
 // module.exports.actionName = function(req,res){
 //     return res.end('<h1>HELLO!</h1>');
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,12 +1,9 @@
 const Post = require('../models/posts');
 
 module.exports.create = function(req, res) {
-  console.log(req.body);
-  console.log(req.user);
-  
   Post.create({
     content: req.body.content,
-    user: req.user._id
+    user: req.user._id 
   })
     .then(post => {
       return res.redirect('back');
