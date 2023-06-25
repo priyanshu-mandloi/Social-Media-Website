e40a75cc3a117d2e639b502c741cdd693d06da54
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-25 20:06:56 +0530

    Formed the deletion of Post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res) {
   Post.create({
@@ -14,3 +15,31 @@ module.exports.create = function(req, res) {
     });
 };
 
+
+// To Delete the Post
+module.exports.destroy = function(req,res){
+  Post.findById(req.params.id)
+    .then(post => {
+      if (!post) {
+        return res.status(404).send("Post not found");
+      }
+      if(post.user.toString() === req.user.id) {
+        post.deleteOne();
+        
+        Comment.deleteMany({ post: req.params.id })
+          .then(() => {
+            return res.redirect('back');
+          })
+          .catch(err => {
+            console.log("Error in deleting comments:", err);
+            return res.status(500).send("Error in deleting comments");
+          });
+      } else {
+        res.redirect('back');
+      }
+    })
+    .catch(err => {
+      console.log("Error in finding post:", err);
+      return res.status(500).send("Error in finding post");
+    });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const postsController = require("../controllers/post_controller");
 
 router.post('/create',passport.checkAuthentication,postsController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
 module.exports = router;
