65f126fa1f4e1c95c7c6211f0a48a67dc6f5c932
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-26 18:17:21 +0530

    Delete operation for the comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -5,7 +5,6 @@ module.exports.create = async function(req, res) {
   try {
     const post = await Post.findById(req.body.post);
     if(post) {
-      console.log("Hello");
       const createdComment= await Comment.create({
         content: req.body.content,
         post: req.body.post,
@@ -21,3 +20,23 @@ module.exports.create = async function(req, res) {
   }
 };
 
+// Deleting a comment
+module.exports.destroy = async function(req, res) {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    if (!comment) {
+      return res.status(404).send("Comment not found");
+    }
+    if (comment.user.toString() === req.user.id) {
+      let postId = comment.post;
+      await comment.deleteOne();
+      await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+      return res.redirect('back');
+    } else {
+      return res.redirect('back');
+    }
+  } catch (err) {
+    console.log("Error in deleting comment:", err);
+    return res.status(500).send("Error in deleting comment");
+  }
+};
