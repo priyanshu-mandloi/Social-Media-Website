cc033380dba548fa194987a5e5ab51cbc12118c5
priyanshu <priyanshumandloigurukul@gmail.com>
2023-06-28 11:13:13 +0530

    Created a Post and Recieved the data using Ajax and also display on the Dom

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
             url:'/posts/create',
             data:newPostForm.serialize(),
             success:function(data){
-                console.log(data);
+               let newPost = newPostDom(data.data.post);
+               $('#post-list-container>ul').prepend(newPost);
             },error : function(error){
                 console.log(error.responseText);
             }    
@@ -21,5 +22,35 @@
 
 
    // method to display the post in DOM.
+   let newPostDom = function(post){
+      return $(
+    `<li id="post-${post._id}">
+      <p>
+        <small>
+          <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+        </small>
+          ${post.content}
+        <br>
+        <small>
+          ${post.user.name}
+       </small>
+      </p>
+      <div class="post-comments">
+       <form action="/comments/create" method="POST">
+           <input type="text" name="content" placeholder="Type Here to add comment... " required>
+           <input type="hidden" name="post" value="${post._id}">
+           <input type="submit" value="Add Comment">
+       </form>
+       <!-- Showing the comments  -->
+       <div class="post-comments-list">
+        <ul id="post-comments-${post._id}">
+
+        </ul>
+       </div>
+      </div>
+    </li>`
+    );
+   }
+
    createPost();
 }
