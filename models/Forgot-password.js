19bbf34e58f3847c059b099b9ff2b89476924ce9
priyanshu <priyanshumandloigurukul@gmail.com>
2023-07-13 11:25:37 +0530

    Added some template for the email

--- models/Forgot-password.js
+++ models/Forgot-password.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 
-const forgotPasswordTokens = new mongoose.Schema({
+const tokenSchema = new mongoose.Schema({
    user:{
     type:mongoose.Schema.Types.ObjectId,
+    required:true,
     ref:'User'
+   },
+   token:{
+    type:String,
+    required:true
+   },
+   createdAt:{
+     type:Date,
+     default:Date.now,
+     expires:3600
    }
-   
-
 });
 
 
-
-const Post = mongoose.model("Forgot",forgotPasswordTokens);
+const Post = mongoose.model("Token",tokenSchema);
 module.exports = Post;
